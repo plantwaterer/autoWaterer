2b43b56144cc721c4a257ed4c90bef565cdf4960
plantwaterer45 <plantwaterer45@gmail.com>
2018-04-21 19:29:44 +0000

    poop

--- moistureTest/main.py
+++ moistureTest/main.py
@@ -2,10 +2,22 @@ import RPi.GPIO as gpio
 import time
 import logging
 
-import Adafruit
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
 # Setup logger
 logger = logging.getLogger('moisture-test-log')
-logger.setLevel(logging.debug)
+logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler('moistureTest.log')
+handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(levelname)10s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+# Setup ADC
+SPI_PORT = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+logger.info("ADC Setup")
+
+
